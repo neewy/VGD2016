f1623bdba1ee0db13edc0072f8e6d68d8456dcd5
MukhinAlexey <mukhinalexeym@gmail.com>
2016-10-23 23:54:58 +0300

    Architecture update

---
+++ IsoGame/Map/MapObject.swift
@@ -0,0 +1,16 @@
+//
+//  MapObject.swift
+//  IsoGame
+//
+//  Created by Alexey on 23.10.16.
+//  Copyright © 2016 Dmitriy Kapitun. All rights reserved.
+//
+
+import UIKit
+
+// Пока не трогаем
+class MapObject: NSObject {
+
+    
+    
+}
