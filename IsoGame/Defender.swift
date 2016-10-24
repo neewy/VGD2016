63bb08f37fd14d7771a5773f3cada543c2482ab3
MukhinAlexey <mukhinalexeym@gmail.com>
2016-10-24 11:56:31 +0300

    Road And Architecture Final

---
+++ IsoGame/Defender.swift
@@ -0,0 +1,13 @@
+//
+//  Tower.swift
+//  IsoGame
+//
+//  Created by Alexey on 24.10.16.
+//  Copyright © 2016 Dmitriy Kapitun. All rights reserved.
+//
+
+import UIKit
+
+class Tower: NSObject {
+
+}
